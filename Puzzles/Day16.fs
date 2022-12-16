36b30806e56bd1af8a334740d2f00e8aec743dfe
markpattison <mr.pattison@gmail.com>
2022-12-16 15:39:44 +0000

    day 16

---
+++ Puzzles/Day16.fs
@@ -0,0 +1,189 @@
+﻿module Day16
+
+open System
+open System.IO
+
+type Valve =
+    {
+        Name: string
+        FlowRate: int
+        TunnelsTo: string[]
+    }
+
+let parseLine (s: string) =
+    let name = s.Substring(6, 2)
+    let semicolon = s.IndexOf(';')
+    let flowRate = s.Substring(23, semicolon - 23) |> int
+    let tunnels =
+        s.Split(',')
+        |> Array.map (fun t -> t.Substring(t.Length - 2))
+    {
+        Name = name
+        FlowRate = flowRate
+        TunnelsTo = tunnels
+    }
+let origValves =
+    File.ReadAllLines(@"input\day16.txt")
+    |> Array.map parseLine
+
+type Environment =
+    {
+        StartValve: int
+        DestinationValveIds: int []
+        Distances: Map<int, int []>
+        FlowRates: int []
+    }
+
+let valveDistances startValve (valves: Valve []) =
+    let valveIds = valves |> Array.mapi (fun i v -> (v.Name, i)) |> Map.ofArray
+    let numValves = valves.Length
+
+    let tunnelsTo =
+        valves
+        |> Array.map (fun v -> v.TunnelsTo |> Array.map (fun t -> valveIds.[t]))
+
+    let originValveIds = valves |> Array.filter (fun v -> v.FlowRate > 0 || v.Name = startValve) |> Array.map (fun v -> valveIds.[v.Name])
+    let destinationValveIds = valves |> Array.filter (fun v -> v.FlowRate > 0) |> Array.map (fun v -> valveIds.[v.Name])
+
+    let distancesFrom start =
+        let mutable unvisited = [0 .. numValves - 1] |> Set.ofList
+        let tentativeDistances = Array.init numValves (fun i -> if i = start then 0 else Int32.MaxValue)
+        let mutable current = start
+        let mutable finished = false
+
+        while not finished do
+            let currentDistance = tentativeDistances.[current]
+            tunnelsTo.[current]
+            |> Array.iter (fun t ->
+                let distanceThroughCurrent = currentDistance + 1
+                if distanceThroughCurrent < tentativeDistances.[t] then tentativeDistances.[t] <- distanceThroughCurrent)
+            
+            unvisited <- Set.remove current unvisited
+
+            if unvisited.IsEmpty then
+                finished <- true
+            else
+                current <-
+                    unvisited
+                    |> Set.toSeq
+                    |> Seq.minBy (fun v -> tentativeDistances.[v])
+
+        tentativeDistances
+
+    let allDistances =
+        originValveIds
+        |> Array.map (fun i -> i, distancesFrom i)
+        |> Map.ofArray
+
+    {
+        StartValve = valveIds.[startValve]
+        DestinationValveIds = destinationValveIds
+        Distances = allDistances
+        FlowRates = valves |> Array.map (fun v -> v.FlowRate)
+    }
+
+type State =
+    {
+        RemainingMinutes: int
+        ReleasedSoFar: int
+        Location: int
+        Unopened: int []
+    }
+
+let rec maxReleased environment state =
+    if state.RemainingMinutes <= 2 || state.Unopened.Length = 0 then
+        state.ReleasedSoFar
+    else
+        let location = state.Location
+        let distancesFromHere = environment.Distances.[location]
+        let canVisit = state.Unopened |> Array.filter (fun v -> distancesFromHere.[v] < state.RemainingMinutes - 1)
+
+        if canVisit.Length = 0 then
+            state.ReleasedSoFar
+        else
+            canVisit
+            |> Array.map (fun dest ->
+                maxReleased
+                    environment
+                    {
+                        RemainingMinutes = state.RemainingMinutes - distancesFromHere.[dest] - 1
+                        ReleasedSoFar = state.ReleasedSoFar + environment.FlowRates.[dest] * (state.RemainingMinutes - distancesFromHere.[dest] - 1)
+                        Location = dest
+                        Unopened = state.Unopened |> Array.except [| dest |]
+                    })
+            |> Array.max
+
+let part1() =
+    let environment = valveDistances "AA" origValves
+    
+    let initialState =
+        {
+            RemainingMinutes = 30
+            ReleasedSoFar = 0
+            Location = environment.StartValve
+            Unopened = environment.DestinationValveIds
+        }
+
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()
+
+    let maxPressure = maxReleased environment initialState
+
+    printfn "Max pressure released: %i" maxPressure
+
+    sw.Stop()
+    printfn "MS: %i" sw.ElapsedMilliseconds
+
+let maxElephant environment unopened =
+    let elephantInitialState =
+        {
+            RemainingMinutes = 26
+            ReleasedSoFar = 0
+            Location = environment.StartValve
+            Unopened = unopened
+        }
+    maxReleased environment elephantInitialState
+
+let rec maxReleasedPlusElephant environment state =
+    if state.RemainingMinutes <= 2 || state.Unopened.Length = 0 then
+        state.ReleasedSoFar + maxElephant environment state.Unopened
+    else
+        let location = state.Location
+        let distancesFromHere = environment.Distances.[location]
+        let canVisit = state.Unopened |> Array.filter (fun v -> distancesFromHere.[v] < state.RemainingMinutes - 1)
+
+        if canVisit.Length = 0 then
+            state.ReleasedSoFar + maxElephant environment state.Unopened
+        else
+            canVisit
+            |> Array.map (fun dest ->
+                maxReleasedPlusElephant
+                    environment
+                    {
+                        RemainingMinutes = state.RemainingMinutes - distancesFromHere.[dest] - 1
+                        ReleasedSoFar = state.ReleasedSoFar + environment.FlowRates.[dest] * (state.RemainingMinutes - distancesFromHere.[dest] - 1)
+                        Location = dest
+                        Unopened = state.Unopened |> Array.except [| dest |]
+                    })
+            |> Array.max
+
+let part2() =
+    let environment = valveDistances "AA" origValves
+
+    let initialState =
+        {
+            RemainingMinutes = 26
+            ReleasedSoFar = 0
+            Location = environment.StartValve
+            Unopened = environment.DestinationValveIds
+        }
+
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()
+
+    let maxPressure = maxReleasedPlusElephant environment initialState
+
+    printfn "Max pressure released: %i" maxPressure
+
+    sw.Stop()
+    printfn "MS: %i" sw.ElapsedMilliseconds
--- Puzzles/Main.fs
+++ Puzzles/Main.fs
@@ -2,77 +2,82 @@
 
 let header s = printfn "\n%s\n" s
 
-header "Day 1"
+//header "Day 1"
 
-Day1.part1()
-Day1.part2()
+//Day1.part1()
+//Day1.part2()
 
-header "Day 2"
+//header "Day 2"
 
-Day2.part1()
-Day2.part2()
+//Day2.part1()
+//Day2.part2()
 
-header "Day 3"
+//header "Day 3"
 
-Day3.part1()
-Day3.part2()
+//Day3.part1()
+//Day3.part2()
 
-header "Day 4"
+//header "Day 4"
 
-Day4.part1()
-Day4.part2()
+//Day4.part1()
+//Day4.part2()
 
-header "Day 5"
+//header "Day 5"
 
-Day5.part1()
-Day5.part2()
+//Day5.part1()
+//Day5.part2()
 
-header "Day 6"
+//header "Day 6"
 
-Day6.part1()
-Day6.part2()
+//Day6.part1()
+//Day6.part2()
 
-header "Day 7"
+//header "Day 7"
 
-Day7.part1()
-Day7.part2()
+//Day7.part1()
+//Day7.part2()
 
-header "Day 8"
+//header "Day 8"
 
-Day8.part1()
-Day8.part2()
+//Day8.part1()
+//Day8.part2()
 
-header "Day 9"
+//header "Day 9"
 
-Day9.part1()
-Day9.part2()
+//Day9.part1()
+//Day9.part2()
 
-header "Day 10"
+//header "Day 10"
 
-Day10.part1()
-Day10.part2()
+//Day10.part1()
+//Day10.part2()
 
-header "Day 11"
+//header "Day 11"
 
-Day11.part1()
-Day11.part2()
+//Day11.part1()
+//Day11.part2()
 
-header "Day 12"
+//header "Day 12"
 
-Day12.part1()
-Day12.part2()
+//Day12.part1()
+//Day12.part2()
 
-header "Day 13"
+//header "Day 13"
 
-Day13.part1()
-Day13.part2()
+//Day13.part1()
+//Day13.part2()
 
-header "Day 14"
+//header "Day 14"
 
-Day14.part1()
-Day14.part2()
+//Day14.part1()
+//Day14.part2()
 
-header "Day 15"
+//header "Day 15"
 
-Day15.part1()
-Day15.part2()
+//Day15.part1()
+//Day15.part2()
+
+header "Day 16"
+
+Day16.part1()
+Day16.part2()
